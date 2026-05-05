545b368f77ed68d872549f7bbd6b7ddb902a644a
resulturfent <rsltrfnt@gmail.com>
2026-05-05 21:25:27 +0300

    Add project files.

---
+++ Ders1_1_MVC/Controllers/UrunlerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ders1_1_MVC.Controllers
+{
+    public class UrunlerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
