4ead24f44ad261489569d16affacd497c3cba73d
resulturfent <rsltrfnt@gmail.com>
2026-05-08 20:21:01 +0300

    razor tekrar işlemi

--- Ders2_MVC_Razor/Controllers/AnasayfaController.cs
+++ Ders2_MVC_Razor/Controllers/AnasayfaController.cs
@@ -13,6 +13,17 @@ namespace Ders2_MVC_Razor.Controllers
 
         public IActionResult Menu()
         {
+            for (int i = 0; i < 10; i++)
+            {
+                
+            }
+
+            if (DateTime.Now.Hour < 12)
+            {
+                ViewBag.mesaj = "Günaydın";
+            }
+            int x = 90;
+            int[] sayilar= new int[10];
             return View();
         }
     }
