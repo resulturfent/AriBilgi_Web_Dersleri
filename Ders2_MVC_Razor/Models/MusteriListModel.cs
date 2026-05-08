999c90a6cad5a7aa2ddf5fcf8729fd540b484a76
resulturfent <rsltrfnt@gmail.com>
2026-05-08 20:44:29 +0300

    Müşter için tekli model ve liste model örnekleri Model, Razor kullanılarak yapıldı

---
+++ Ders2_MVC_Razor/Models/MusteriListModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Ders2_MVC_Razor.Models
+{
+    public class MusteriListModel
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public string Telefon { get; set; }
+        public string Adres { get; set; }
+        public string Il { get; set; }
+        public string Ilce { get; set; }
+
+    }
+}
