2a2f2cf6213225197fccc22bca40613d493e7f07
resulturfent <rsltrfnt@gmail.com>
2026-05-08 19:12:26 +0300

    Razor proje oluşturuldu

---
+++ Ders2_MVC_Razor/Controllers/AnasayfaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ders2_MVC_Razor.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Menu()
+        {
+            return View();
+        }
+    }
+}
