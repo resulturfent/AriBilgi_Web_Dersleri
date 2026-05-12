14720a0a3d19e33ef87d313e0d88cf770d2661b0
resulturfent <rsltrfnt@gmail.com>
2026-05-12 20:49:27 +0300

    Layout işlendi

---
+++ Ders3_1_Layout/Controllers/HakkimizdaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ders3_1_Layout.Controllers
+{
+    public class HakkimizdaController : Controller
+    {
+        /*
+         https://bootsnipp.com/snippets/pj2aa
+         */
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
